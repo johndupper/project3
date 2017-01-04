089cae703dde4957a8011e28d7746bba0af0d494
John Dupper <john.dupper@me.com>
2017-01-04 14:54:18 -0500

    added mongo, mongoose, promises

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//* we added these
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+
+//* Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/project3');
+}
+mongoose.connection.on('error', function(err) {
+        console.error('MongoDB connection error: ' + err);
+        process.exit(-1);
+    }
+);
+mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+});
+
+
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
