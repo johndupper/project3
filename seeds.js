b4326de381e36d6aa636031875478bc335f4cae2
John Dupper <john.dupper@me.com>
2017-01-04 15:22:33 -0500

    catch up commit, no major changes

---
+++ seeds.js
@@ -0,0 +1,33 @@
+//* required for this seeds file to function
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+var User = require('./models/user');
+
+//* we added this
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect('mongodb://localhost/project3');
+}
+mongoose.connection.on('error', function(err) {
+        console.error('MongoDB connection error: ' + err);
+        process.exit(-1);
+    }
+);
+mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+});
+
+//* we need to disconnect when the seeds file is done
+function quit() {
+    console.log('mongoose disconnecting called');
+    mongoose.disconnect();
+}
+
+function handleError(error) {
+    console.log('seeds error: ', error);
+    quit();
+    return error;
+}
+
