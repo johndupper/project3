55fff0c30a8e80d543f5829ca1493e3ab9121b61
John Dupper <john.dupper@me.com>
2017-01-04 15:06:35 -0500

    created user mongoose schema

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    local: {
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
