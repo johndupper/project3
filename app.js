9c1afd3e52eb5c74c5f5a4d6106d9fc3545771b2
John Dupper <john.dupper@me.com>
2017-01-05 15:00:38 -0500

    maintenence commit

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// add path for angular ui.router here!
 
 app.use('/', index);
 app.use('/users', users);
