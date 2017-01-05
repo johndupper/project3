3eff44a81eb13d5a97d213e4218ded28b65b725b
John Dupper <john.dupper@me.com>
2017-01-05 15:16:51 -0500

    established placeholder for potential comments

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
     local: {
@@ -9,10 +10,19 @@ var UserSchema = new mongoose.Schema({
         password: {
             type: String,
             required: true
-        }
+        },
+        // comments: {}
     }
 }, {
     timestamps: true
 });
+
+UserSchema.methods.encrypt = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+UserSchema.methods.isValidPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
