7b3b4d80ef3e9935883542f7370da395f9ec85ac
John Dupper <john.dupper@me.com>
2017-01-06 11:37:38 -0500

    added authentication middleware to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+function authenticate(req, res, next) {
+    if(!req.isAuthenticated()) {
+        req.flash('error', 'Please signup or login.');
+        res.redirect('/');
+    }
+    else {
+        next();
+    }
+}
 
 router.get('/', function(req, res, next) {
     res.send('test');
