4a4a495deedc278489b526c04cc3e60238c3e8a1
John Dupper <john.dupper@me.com>
2017-01-06 15:58:03 -0500

    router GET for index functional

--- routes/profile.js
+++ routes/profile.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+
+var Job = require('../models/job');
 
 function authenticate(req, res, next) {
     if(!req.isAuthenticated()) {
         req.flash('error', 'Please signup or login.');
         res.redirect('/');
-    }
-    else {
+    } else {
         next();
     }
 }
 
-router.get('/', function(req, res, next) {
-    res.send('test');
+router.get('/', authenticate, function(req, res, next) {
+    Job.find({ jobtitle: 'JobTitle' })
+        .then(function(job) {
+            console.log(job);
+            res.render('profile', {
+                job: job
+            });
+        });
 });
 
 module.exports = router;
