c8c12cf4d43542d91fdeed8c8c27befd4b8c92d2
John Dupper <john.dupper@me.com>
2017-01-07 18:02:47 -0500

    created search route, querying api hardcoded

--- routes/search.js
+++ routes/search.js
@@ -17,4 +17,17 @@ router.get('/', authenticate, function(req, res, next) {
     res.render('search');
 });
 
+router.get('/results', authenticate, function(req, res, next) {
+    request('http://api.indeed.com/ads/apisearch?publisher=9447015102421242&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json',
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var testJSON = JSON.parse(body);
+                console.log(testJSON);
+                res.render('results', {
+                    api: testJSON
+                });
+            }
+        });
+});
+
 module.exports = router;
