76564c04f8e4c7d8b3c87b3295f664e03eb82b09
John Dupper <john.dupper@me.com>
2017-01-09 19:53:48 -0500

    changed routing strategy to landing page for sign up, sign in

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+/*  ROUTES:
+*   1. index (landing page)
+*   2. sign up (GET)
+*   3. sign up (POST)
+*   4. log in (GET)
+*   5. log in (POST)
+*   6. log out (GET)
+*/
 
-router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Express',
-        message: req.flash()
-    });
+router.get('/', function(req, res) {
+    res.render('landing');
 });
 
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function(req, res) {
     res.render('signup', {
         message: req.flash()
     });
@@ -19,13 +24,14 @@ router.get('/signup', function(req, res, next) {
 router.post('/signup', function(req, res, next) {
     var signupStrategy = passport.authenticate('local-signup', {
         successRedirect: '/profile',
+        successRedirect: '/templates/',
         failureRedirect: '/signup',
         failureFlash: true
     });
     return signupStrategy(req, res, next);
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function(req, res) {
     res.render('login', {
         message: req.flash()
     });
@@ -40,7 +46,7 @@ router.post('/login', function(req, res, next) {
     return loginProperty(req, res, next);
 });
 
-router.get('/logout', function(req, res, next) {
+router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
 });
